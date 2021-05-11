6e9e2967ef594abc4858d8349f430a7a1aafd39a
brunadossantos-tech <74012238+brunadossantos-tech@users.noreply.github.com>
2021-05-11 12:09:21 +0200

    add icon and update css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import logo from './logo.png';
 import './App.css';
 import Dictionary from "./Dictionary";
+import language from "./language.png";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
       <header className="App-header">
-        <div className="row d-flex justify-content-evenly">
-          <div className="col">
             <img src={logo} className="App-logo img-fluid" alt="logo" />
-          </div>
-          <div>
-            <h1 className="col">
-          Dictionary App
-          </h1>
-          </div>
-        </div>
+            <h1>
+              Dictionary App
+              <span className="icon">
+                <img src={language} alt="language" />
+              </span>
+              </h1>
+          
       </header>
       <main>
         <Dictionary />
       </main>
       <footer>
-        Coded by Bruna Santos
+        Coded by {""}
+        <a href="https://practical-goldberg-8caec7.netlify.app/" target="_blank" rel="noreferrer">Bruna Santos</a> and is open-sourced on{" "}
+        <a href="https://github.com/brunadossantos-tech/dictionary-project.git" target="_blank" rel="noreferrer">GitHub</a> and hosted on{" "}
+        <a href="https://angry-kowalevski-094529.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
       </footer>
       </div>
     </div>
   );
 }
+
